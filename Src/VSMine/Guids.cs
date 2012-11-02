e9bc6e6b55fecb9da2e391a69f5b3641a9f10233
Biegal <marcin@biegala.net>
2012-11-02 06:40:22 +0100

    Initial commit with project.

---
+++ Src/VSMine/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace KoiSoft.VSMine
+{
+    static class GuidList
+    {
+        public const string guidVSMinePkgString = "c5aa3c12-65a6-4fb0-982c-59c3e50cccc7";
+        public const string guidVSMineCmdSetString = "07fac7fa-269a-42ab-aa7f-3e89ebf47bc2";
+        public const string guidToolWindowPersistanceString = "15ce9f51-b9aa-4dfb-845d-8cdb56bb3329";
+
+        public static readonly Guid guidVSMineCmdSet = new Guid(guidVSMineCmdSetString);
+    };
+}
---
+++ Src/VSMine/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace KoiSoft.VSMine
+{
+    static class PkgCmdIDList
+    {
+
+        public const uint cmdidRemineTasks =    0x101;
+
+    };
+}
